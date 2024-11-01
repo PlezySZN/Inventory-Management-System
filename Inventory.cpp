a67489fe2e3540cafe3885e74006f6c617e5fdb4
PlezySZN <yomosantiago15@gmail.com>
2024-10-31 23:31:13 -0400

    1. Prettier Format

--- Inventory.cpp
+++ Inventory.cpp
@@ -1,126 +1,141 @@
-#include <iostream> 
-#include <iomanip> 
-#include <string> 
-#include <algorithm> 
-#include <vector> 
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <algorithm>
+#include <vector>
 
-    using std::vector, std::string, std::cout, std::cin, std::endl;
+using std::vector, std::string, std::cout, std::cin, std::endl;
 
-    struct Product{
-        string idNum;
-        string name;
-        string category;
-        double price;
-        double stock;
-    } ;
-    //Keven Paulino Ferrer
-    void addProduct(vector <Product> &userProducts) {
-        Product newProduct;
-        char continuar;
-        do{
-            cout << "\n===========================================\n";
-            cout << "========Inventory Management System========\n";
-            cout << "=============Adding New Product=============\n";    
+struct Product
+{
+    string idNum;
+    string name;
+    string category;
+    double price;
+    double stock;
+};
+// Keven Paulino Ferrer
+void addProduct(vector<Product> &userProducts)
+{
+    Product newProduct;
+    char continuar;
+    do
+    {
+        cout << "\n===========================================\n";
+        cout << "========Inventory Management System========\n";
+        cout << "=============Adding New Product=============\n";
 
-            do {
+        do
+        {
             cout << "Enter Identification Number (----): ";
             std::getline(cin, newProduct.idNum);
 
-                if (newProduct.idNum.length() > 4) {
-                    cout <<"\n===========================================\n";
-                    cout << "=====ERROR: ID Can't be more than 9999=====\n";
-                    cout <<"===========================================\n";
-                    continue;
+            if (newProduct.idNum.length() > 4)
+            {
+                cout << "\n===========================================\n";
+                cout << "=====ERROR: ID Can't be more than 9999=====\n";
+                cout << "===========================================\n";
+                continue;
+            }
+            else if (newProduct.idNum.length() < 4)
+            {
+                cout << "\n============================================\n";
+                cout << "======ERROR: ID Can't be less than 1000====== \n";
+                cout << "============================================\n";
+                continue;
+            }
 
-                    } else if (newProduct.idNum.length() < 4) {
-                        cout << "\n============================================\n";
-                        cout << "======ERROR: ID Can't be less than 1000====== \n";
-                        cout << "============================================\n";
-                        continue;
-                }
+        } while (newProduct.idNum.length() < 4);
 
-            } while (newProduct.idNum.length() < 4);
+        cout << "Enter Name of the Product: ";
+        std::getline(cin, newProduct.name);
+        cout << "Enter the category (food, drink, etc...): ";
+        std::getline(cin, newProduct.category);
 
-            cout << "Enter Name of the Product: ";
-            std::getline (cin, newProduct.name);
-            cout << "Enter the category (food, drink, etc...): ";
-            std::getline (cin, newProduct.category);
+        do
+        {
+            cout << "Enter the Price of the Product:";
+            cin >> newProduct.price;
 
-            do {
-                cout << "Enter the Price of the Product:";
-                cin>> newProduct.price;
-                
-                if (!(cin, newProduct.price)) {
-                        cout << "\n===========================================\n";
-                        cout << "=====ERROR: Invalid input. Please Enter a number ===== \n";
-                        cout << "===========================================\n";
+            if (!(cin, newProduct.price))
+            {
+                cout << "\n===========================================\n";
+                cout << "=====ERROR: Invalid input. Please Enter a number ===== \n";
+                cout << "===========================================\n";
 
-                        cin.clear();
-                        cin.ignore();
-                        
-                        continue;
-                    }
-                        if (newProduct.price<0) {
-                            cout << "\n===========================================\n";
-                            cout << "=====ERROR: Price Can't be Less than 0===== \n";
-                            cout << "===========================================\n";
-                        }
-            } while (newProduct.price<0);
+                cin.clear();
+                cin.ignore();
 
-            do {
-                cout << "Enter the Amount in Stock:";
-                cin>> newProduct.stock;
+                continue;
+            }
+            if (newProduct.price < 0)
+            {
+                cout << "\n===========================================\n";
+                cout << "=====ERROR: Price Can't be Less than 0===== \n";
+                cout << "===========================================\n";
+            }
+        } while (newProduct.price < 0);
 
-                if (!(cin, newProduct.price)) {
-                        cout << "\n===========================================\n";
-                        cout << "=====ERROR: Invalid input. Please Enter a number ===== \n";
-                        cout << "===========================================\n";
+        do
+        {
+            cout << "Enter the Amount in Stock:";
+            cin >> newProduct.stock;
 
-                        cin.clear();
-                        cin.ignore();
-                        
-                        continue;
-                    }
+            if (!(cin, newProduct.price))
+            {
+                cout << "\n===========================================\n";
+                cout << "=====ERROR: Invalid input. Please Enter a number ===== \n";
+                cout << "===========================================\n";
 
-                        if (newProduct.stock<0) {
-                            cout << "\n============================================\n";
-                            cout << "======ERROR: Stock Can't be Less tha 0====== \n";
-                            cout << "============================================\n";
-                        }
+                cin.clear();
+                cin.ignore();
 
-            } while (newProduct.stock<0);
+                continue;
+            }
 
-            userProducts.push_back(newProduct);  
-            
-            cout<< "Do You Wish to Continue Adding Products (Y/N)";
-            cin >> continuar;
-            cin.ignore();
-            continuar = toupper(continuar); 
-            if (continuar=='N') {
-                break;
-            } 
-        } while (continuar=='Y');     
-    }
+            if (newProduct.stock < 0)
+            {
+                cout << "\n============================================\n";
+                cout << "======ERROR: Stock Can't be Less tha 0====== \n";
+                cout << "============================================\n";
+            }
 
-    void deleteProduct() {
-    }
+        } while (newProduct.stock < 0);
 
-    //Harrys Santiago Santana
-    void updateProduct(vector <Product> &userProducts) {
+        userProducts.push_back(newProduct);
+
+        cout << "Do You Wish to Continue Adding Products (Y/N)";
+        cin >> continuar;
+        cin.ignore();
+        continuar = toupper(continuar);
+        if (continuar == 'N')
+        {
+            break;
+        }
+    } while (continuar == 'Y');
+}
+
+void deleteProduct()
+{
+}
+
+// Harrys Santiago Santana
+void updateProduct(vector<Product> &userProducts)
+{
     string id;
 
     cout << "\n===========================================\n";
     cout << "========Inventory Management System========\n";
-    cout << "==============Updating Product==============\n"; 
+    cout << "==============Updating Product==============\n";
     cout << "Enter Product ID to update: ";
     cin >> id;
     cin.ignore();
 
-    auto it = find_if(userProducts.begin(), userProducts.end(),[&id](Product &matchedProduct) {
-        return matchedProduct.idNum == id;
-    });
+    auto it = find_if(userProducts.begin(), userProducts.end(), [&id](Product &matchedProduct)
+                      { return matchedProduct.idNum == id; });
 
-    if (it != userProducts.end()) {
+    if (it != userProducts.end())
+    {
         string newName;
         string newCategory;
         double newStock;
@@ -132,9 +147,12 @@
         cout << "Enter product name to update: ";
         std::getline(cin, newName);
 
-        if (!newName.empty()) {
+        if (!newName.empty())
+        {
             it->name = newName;
-        } else {
+        }
+        else
+        {
             cout << "No changes made." << endl;
         }
 
@@ -144,9 +162,12 @@
         cout << "Enter product category to update: ";
         std::getline(cin, newCategory);
 
-        if (!newCategory.empty()) {
+        if (!newCategory.empty())
+        {
             it->category = newCategory;
-        } else {
+        }
+        else
+        {
             cout << "No changes made." << endl;
         }
 
@@ -156,15 +177,17 @@
         cout << "Enter price to update: ";
         cin >> newPrice;
 
-        if (newPrice < 0) {
+        if (newPrice < 0)
+        {
             cout << "\n=======================================\n";
             cout << "=====ERROR: Price Can't be Less 0===== \n";
             cout << "=========================================";
-    }
+        }
 
-            if (!newPrice < 0) {
-                it->price = newPrice;
-            }
+        if (!newPrice < 0)
+        {
+            it->price = newPrice;
+        }
 
         cout << "===========================================\n";
         cout << "Current stock: " << it->stock << endl;
@@ -173,86 +196,92 @@
         cin >> newStock;
         it->stock = newStock;
 
-        
-        if (newStock < 0) {
+        if (newStock < 0)
+        {
             cout << "\n=======================================\n";
             cout << "=====ERROR: Stock Can't be Less 0===== \n";
             cout << "=========================================";
+        }
+
+        if (!newStock < 0)
+        {
+            it->stock = newStock;
+        }
     }
-
-            if (!newStock < 0) {
-                it->stock = newStock;
-            }
-
-    } else {
+    else
+    {
         cout << "\n=======================================\n";
         cout << "=========ERROR: ID not found=========\n";
         cout << "=========================================\n";
     }
-
 }
-    int searchProduct() {
-        return 0;
+int searchProduct()
+{
+    return 0;
+}
+
+void printInventory(vector<Product> &userProducts)
+{
+
+    std::cout << std::left << std::setw(10) << "ID"
+              << std::setw(10) << "Name"
+              << std::setw(15) << "Category"
+              << std::setw(15) << "Stock"
+              << std::setw(15) << "Price"
+              << std::endl;
+
+    for (auto &product : userProducts)
+    {
+
+        std::cout << std::setw(10) << product.idNum
+                  << std::setw(10) << product.name
+                  << std::setw(15) << product.category
+                  << std::setw(15) << product.stock
+                  << std::setw(0) << "$"
+                  << std::setw(20) << std::fixed << std::setprecision(2)
+                  << product.price
+                  << std::endl;
     }
+}
+int main()
+{
+    int menuChoice;
+    vector<Product> userProducts;
+    do
+    {
+        cout << "\n============================================\n";
+        cout << "========Inventory Management System=========\n";
+        cout << "===============Inventory Menu===============\n";
+        cout << "1. Add a new product\n";
+        cout << "2. Delete a product\n";
+        cout << "3. Update a product\n";
+        cout << "4. Search for a product\n";
+        cout << "5. Print inventory\n";
+        cout << "6. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> menuChoice;
+        cin.ignore();
 
-    void printInventory(vector <Product> &userProducts) {
+        switch (menuChoice)
+        {
 
-            std::cout << std::left << std::setw(10) << "ID" 
-            << std::setw(10) << "Name" 
-            << std::setw(15) << "Category" 
-            << std::setw(15) << "Stock" 
-            << std::setw(15) << "Price" 
-            << std::endl;
+        case 1:
+            addProduct(userProducts);
+            break;
+        case 2:
+            deleteProduct();
+            break;
+        case 3:
+            updateProduct(userProducts);
+            break;
+        case 4:
+            searchProduct();
+            break;
+        case 5:
+            printInventory(userProducts);
+            break;
+        }
 
-        for (auto &product : userProducts) {
-            
-            std::cout << std::setw(10) << product.idNum 
-            << std::setw(10) << product.name 
-            << std::setw(15) << product.category 
-            << std::setw(15) << product.stock 
-            << std::setw(0)  << "$" 
-            << std::setw(20) << std::fixed << std::setprecision(2) 
-            << product.price
-            << std::endl;
-
-        }   
-    }
-    int main () {
-        int menuChoice;
-        vector <Product> userProducts;
-        do {
-            cout << "\n============================================\n";
-            cout << "========Inventory Management System=========\n";
-            cout << "===============Inventory Menu===============\n";
-            cout << "1. Add a new product\n";
-            cout << "2. Delete a product\n";
-            cout << "3. Update a product\n";
-            cout << "4. Search for a product\n";
-            cout << "5. Print inventory\n";
-            cout << "6. Exit\n";
-            cout << "Enter your choice: ";
-            cin >> menuChoice;
-            cin.ignore();
-            
-            switch (menuChoice){
-
-            case 1:
-                addProduct(userProducts);
-                break;
-            case 2:
-                deleteProduct();
-                break;
-            case 3:
-                updateProduct(userProducts);
-                break;
-            case 4:
-                searchProduct();
-                break;
-            case 5:
-                printInventory(userProducts);
-                break;
-            }
-            
-        } while(menuChoice !=6);
-        return 0;
-    }
+    } while (menuChoice != 6);
+    return 0;
+}
